b8cfa07d09821d119f55c319b574a312b0855003
yoshoku <yoshoku@outlook.com>
2025-12-16 12:39:39 +0900

    refactor: summarize application style

--- src/App.vue
+++ src/App.vue
@@ -148,18 +148,6 @@ onUnmounted(() => {
   </div>
 </template>
 
-<style>
-html,
-body,
-#app {
-  margin: 0;
-  padding: 0;
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
-}
-</style>
-
 <style scoped>
 .container {
   display: flex;
